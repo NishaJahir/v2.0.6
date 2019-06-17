612e5b0d6947280d9ac0c8db42a566cea3b8067b
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-06-17 19:35:56 +0530

    Update novalnet_sepa.js

--- resources/js/novalnet_sepa.js
+++ resources/js/novalnet_sepa.js
@@ -6,7 +6,10 @@ jQuery(document).ready( function() {
 		console.log(iban);
 	});
 	jQuery('#nn_sepa_form').on('submit',function(){
+		if( jQuery('#nn_sepa_iban') != '' || jQuery( '#nn_sepa_cardholder' ) != '' )
+		{
 		$('#novalnet_form_btn').attr('disabled',true);
+		}
 	});
   
   jQuery( '#nn_sepa_cardholder' ).keypress(
