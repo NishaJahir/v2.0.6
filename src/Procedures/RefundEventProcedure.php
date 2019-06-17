4ac65a31f98abc9268c2e95e730729a8fb124570
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-06-17 15:40:59 +0530

    Update RefundEventProcedure.php

--- src/Procedures/RefundEventProcedure.php
+++ src/Procedures/RefundEventProcedure.php
@@ -77,6 +77,7 @@ class RefundEventProcedure
 	 
 	   $payments = pluginApp(\Plenty\Modules\Payment\Contracts\PaymentRepositoryContract::class);  
        $paymentDetails = $payments->getPaymentsByOrderId($order->id);
+	     $this->getLogger(__METHOD__)->error('refundinitila', $paymentDetails);
 	   $orderAmount = (float) $order->amounts[0]->invoiceTotal;
 	   $paymentKey = $paymentDetails[0]->method->paymentKey;
 	   $key = $this->paymentService->getkeyByPaymentKey($paymentKey);
